05805f32e530710044e8cffa68abb0d2762cf0f7
Rizka Fajar Nugraha <nunutech4.0@gmail.com>
2026-04-29 09:02:42 +0700

    fix: add UNUserNotificationCenter delegate for iOS foreground notifications & fix Android desugaring version

--- lib/core/data/repositories/notification_repository_impl.dart
+++ lib/core/data/repositories/notification_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+import 'dart:ui';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:news_app/core/domain/repositories/notification_repository.dart';
 
@@ -17,7 +19,8 @@ class NotificationRepositoryImpl implements NotificationRepository {
       requestSoundPermission: true,
     );
 
-    const InitializationSettings initializationSettings = InitializationSettings(
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
       android: initializationSettingsAndroid,
       iOS: initializationSettingsDarwin,
     );
@@ -44,14 +47,28 @@ class NotificationRepositoryImpl implements NotificationRepository {
     required String body,
     String? payload,
   }) async {
-    const AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
-      'news_app_general', // ID Channel
-      'Notifikasi Umum',  // Nama Channel (muncul di Settings OS)
-      channelDescription: 'Channel ini digunakan untuk notifikasi profil dan info umum.',
+    final AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+      'news_app_brutal', 
+      'Notifikasi Darurat',  
+      channelDescription: 'Channel ini tidak bisa diabaikan.',
       importance: Importance.max,
-      priority: Priority.high,
+      priority: Priority.max, // MAKSIMAL
       playSound: true,
       enableVibration: true,
+      vibrationPattern: Int64List.fromList([0, 1000, 500, 2000, 500, 3000]), // GETARAN POLA
+      enableLights: true,
+      ledColor: const Color.fromARGB(255, 255, 0, 0), // KEDIP MERAH
+      ledOnMs: 1000,
+      ledOffMs: 500,
+      styleInformation: const BigTextStyleInformation(
+        'INI ADALAH TEKS PANJANG YANG TIDAK BISA DITUTUPI. BACA SEKARANG JUGA ATAU HP INI AKAN MELEDAK! 🔥🔥🔥',
+        htmlFormatBigText: true,
+        contentTitle: '<b>URGENT ALERT!</b>',
+        htmlFormatContentTitle: true,
+        summaryText: 'Darurat Profil',
+      ),
+      fullScreenIntent: true, // MAKSA BUKA LAYAR
     );
 
     const DarwinNotificationDetails darwinDetails = DarwinNotificationDetails(
@@ -60,7 +77,7 @@ class NotificationRepositoryImpl implements NotificationRepository {
       presentSound: true,
     );
 
-    const NotificationDetails notificationDetails = NotificationDetails(
+    final NotificationDetails notificationDetails = NotificationDetails(
       android: androidDetails,
       iOS: darwinDetails,
     );
